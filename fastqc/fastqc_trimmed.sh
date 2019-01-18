90b5745ecc84422f50a97febf6f7523a9346e668
golden75 <neranjan007@gmail.com>
2019-01-18 00:32:26 -0500

    add fastqc script and folder

---
+++ fastqc/fastqc_trimmed.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+#SBATCH --job-name=fastqc_trimmed
+#SBATCH -n 1
+#SBATCH -N 1
+#SBATCH -c 8
+#SBATCH --mem=10G
+#SBATCH --partition=mcbstudent
+#SBATCH --qos=mcbstudent
+#SBATCH --mail-type=ALL
+#SBATCH --mail-user=first.last@uconn.edu
+#SBATCH -o %x_%j.out
+#SBATCH -e %x_%j.err
+
+echo `hostname`
+
+#################################################################
+# FASTQC of raw reads 
+#################################################################
+dir="after"
+if [ ! -d "$dir" ]; then
+        mkdir -p $dir
+fi
+
+module load fastqc/0.11.5
+fastqc --outdir ./"$dir"/ ../quality_control/trimmed_LB2A_SRR1964642.fastq -t 8
+fastqc --outdir ./"$dir"/ ../quality_control/trimmed_LB2A_SRR1964643.fastq -t 8
+fastqc --outdir ./"$dir"/ ../quality_control/trimmed_LC2A_SRR1964644.fastq -t 8
+fastqc --outdir ./"$dir"/ ../quality_control/trimmed_LC2A_SRR1964645.fastq -t 8
+
