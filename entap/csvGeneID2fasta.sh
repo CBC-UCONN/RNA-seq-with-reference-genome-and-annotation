376ba7945c73cf62406cedb30491e239fde7098a
golden75 <neranjan007@gmail.com>
2019-01-18 10:29:09 -0500

    add entap scripts

---
+++ entap/csvGeneID2fasta.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+#SBATCH --job-name=csvGeneID2fasta
+#SBATCH -n 1
+#SBATCH -N 1
+#SBATCH -c 1
+#SBATCH --mem=1G
+#SBATCH --mail-type=ALL
+#SBATCH -o %x_%j.out
+#SBATCH -e %x_%j.err
+
+echo `hostname`
+
+#head -n 10 Croaker_DESeq2-results-with-normalized.csv > temp.csv
+
+module load biopython/1.70
+
+python csvGeneID2fasta.py temp.csv ProteinTable12197_229515.txt GCF_000972845.1_L_crocea_1.0_protein.faa > GeneID_proteinID.txt
+
---
+++ entap/entap.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+#SBATCH --job-name=entap
+#SBATCH -n 1
+#SBATCH -N 1
+#SBATCH -c 16
+#SBATCH --mem=50G
+#SBATCH --partition=mcbstudent
+#SBATCH --qos=mcbstudent
+#SBATCH --mail-type=ALL
+#SBATCH --mail-user=first.last@uconn.edu
+#SBATCH -o %x_%j.out
+#SBATCH -e %x_%j.err
+
+echo `hostname`
+
+module load anaconda/2.4.0
+module load perl/5.24.0
+module load diamond/0.9.19
+module load eggnog-mapper/0.99.1
+module load interproscan/5.25-64.0
+
+/UCHC/LABS/Wegrzyn/EnTAP/EnTAP --runP -i fasta_out.fasta -d /isg/shared/databases/Diamond/RefSeq/vertebrate_other.protein.faa.88.dmnd -d /isg/shared/databases/Diamond/Uniprot/uniprot_sprot.dmnd --ontology 0  --threads 16 
+
+
