68c2e9a60fa75667bd6b53f6d4bb00b7946d5bd3
Noah Reid <noah.reid@gmail.com>
2020-05-25 16:22:37 -0400

    align

--- align/align.sh
+++ align/align.sh
@@ -18,19 +18,19 @@ echo `hostname`
 #################################################################
 module load hisat2/2.2.0
 
-hisat2 -p 8 --dta -x ../index/L_crocea -q ../quality_control/LB2A_SRR1964642_trim.fastq.gz | \
+hisat2 -p 8 --dta -x ../index/L_crocea -U ../quality_control/LB2A_SRR1964642_trim.fastq.gz | \
 	samtools view -S -h -u - | \
 	samtools sort -T SRR1964642 - >LB2A_SRR1964642.bam
 
-hisat2 -p 8 --dta -x ../index/L_crocea -q ../quality_control/LB2A_SRR1964643_trim.fastq.gz | \
+hisat2 -p 8 --dta -x ../index/L_crocea -U ../quality_control/LB2A_SRR1964643_trim.fastq.gz | \
 	samtools view -S -h -u - | \
 	samtools sort -T SRR1964643 - >LB2A_SRR1964643.bam
 
-hisat2 -p 8 --dta -x ../index/L_crocea -q ../quality_control/LC2A_SRR1964644_trim.fastq.gz | \
+hisat2 -p 8 --dta -x ../index/L_crocea -U ../quality_control/LC2A_SRR1964644_trim.fastq.gz | \
 	samtools view -S -h -u - | \
 	samtools sort -T SRR1964644 - >LC2A_SRR1964644.bam
 
-hisat2 -p 8 --dta -x ../index/L_crocea -q ../quality_control/LC2A_SRR1964645_trim.fastq.gz | \
+hisat2 -p 8 --dta -x ../index/L_crocea -U ../quality_control/LC2A_SRR1964645_trim.fastq.gz | \
 	samtools view -S -h -u - | \
 	samtools sort -T SRR1964645 - >LC2A_SRR1964645.bam
 
