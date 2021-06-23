c235ebf706890e9ae4a0cf0a83de9b9b7095082f
golden75 <neranjan007@gmail.com>
2021-06-23 12:10:24 -0400

    add new align.sh

--- align/align.sh
+++ align/align.sh
@@ -16,31 +16,32 @@ echo `hostname`
 #################################################################
 # Aligning to Genome
 #################################################################
-module load hisat2/2.2.0
-module load samtools/1.9
+module load hisat2/2.2.1
+module load samtools/1.10
 
-hisat2 -p 8 --dta -x ../index/L_crocea -U ../quality_control/LB2A_SRR1964642_trim.fastq.gz | \
-	samtools view -S -h -u - | \
-	samtools sort -T SRR1964642 - >LB2A_SRR1964642.bam
+hisat2 -p 8 -x ../index/L_crocea -U ../quality_control/LB2A_SRR1964642_trim.fastq.gz | \
+	samtools view -@ 8 -S -h -u - | \
+	samtools sort -@ 8 -T SRR1964642 - >LB2A_SRR1964642.bam
 
-hisat2 -p 8 --dta -x ../index/L_crocea -U ../quality_control/LB2A_SRR1964643_trim.fastq.gz | \
-	samtools view -S -h -u - | \
-	samtools sort -T SRR1964643 - >LB2A_SRR1964643.bam
+hisat2 -p 8 -x ../index/L_crocea -U ../quality_control/LB2A_SRR1964643_trim.fastq.gz | \
+	samtools view -@ 8 -S -h -u - | \
+	samtools sort -@ 8 -T SRR1964643 - >LB2A_SRR1964643.bam
 
-hisat2 -p 8 --dta -x ../index/L_crocea -U ../quality_control/LC2A_SRR1964644_trim.fastq.gz | \
-	samtools view -S -h -u - | \
-	samtools sort -T SRR1964644 - >LC2A_SRR1964644.bam
+hisat2 -p 8 -x ../index/L_crocea -U ../quality_control/LC2A_SRR1964644_trim.fastq.gz | \
+	samtools view -@ 8 -S -h -u - | \
+	samtools sort -@ 8 -T SRR1964644 - >LC2A_SRR1964644.bam
 
-hisat2 -p 8 --dta -x ../index/L_crocea -U ../quality_control/LC2A_SRR1964645_trim.fastq.gz | \
-	samtools view -S -h -u - | \
-	samtools sort -T SRR1964645 - >LC2A_SRR1964645.bam
+hisat2 -p 8 -x ../index/L_crocea -U ../quality_control/LC2A_SRR1964645_trim.fastq.gz | \
+	samtools view -@ 8 -S -h -u - | \
+	samtools sort -@ 8 -T SRR1964645 - >LC2A_SRR1964645.bam
 
 
 # index bam files
-samtools index LB2A_SRR1964642.bam
-samtools index LB2A_SRR1964643.bam
-samtools index LC2A_SRR1964644.bam
-samtools index LC2A_SRR1964645.bam
+samtools index LB2A_SRR1964642.bam LB2A_SRR1964642.bai
+samtools index LB2A_SRR1964643.bam LB2A_SRR1964643.bai
+samtools index LC2A_SRR1964644.bam LC2A_SRR1964644.bai
+samtools index LC2A_SRR1964645.bam LC2A_SRR1964645.bai
+
 
 
 
