6100551d844de7aa199663600de2284604048d24
Noah Reid <noah.reid@gmail.com>
2022-12-08 13:45:41 -0500

    Update 01_fasterq_dump.sh

--- 01_raw_data/01_fasterq_dump.sh
+++ 01_raw_data/01_fasterq_dump.sh
@@ -25,9 +25,6 @@ module load sratoolkit/3.0.1
 # The data are a subset (2 populations) from this study:
     # https://www.ncbi.nlm.nih.gov/geo/query/acc.cgi?acc=GSE156460
 
-# you need to have sratoolkit installed
-    # easiest to do this locally
-    # https://github.com/ncbi/sra-tools
 
 ACCLIST=accessionlist.txt
 
